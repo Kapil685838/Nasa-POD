4945466bf60a92c66f25734f61f29c0bd43f57e5
Kapil Chaudhary <kc685838@gmail.com>
2023-04-05 19:11:44 +0530

    search history updated

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function getImageOfTheDay(event) {
 }
 
 // function to save the data in local storage
-let history = [];
+let history = JSON.parse(localStorage.getItem('History')) || [];
 function saveSearch(date){
     history.push({date: `${date}`});
     localStorage.setItem("History", JSON.stringify(history));
@@ -93,5 +93,7 @@ function addSearchToHistory(){
 
 // program start from here
 document.getElementById('search').addEventListener('click', getImageOfTheDay);
-// function call for current image when page loads.
+
+// function calls when page loads.
 window.addEventListener('load', getCurrentImageOfTheDay);
+window.addEventListener('load', addSearchToHistory);
